0c61f3f7f395da21d0dbf34d4f0424defd69aedd
iamkaizen <kimsokhom654@gmail.com>
2025-04-30 09:54:55 +0700

    Finish CRUD operation

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -18,7 +18,7 @@ const Home = ({ posts }) => {
             <Head>
                 <title>{component}</title>
             </Head>
-            <h1 className="title">Hello</h1>
+            <h1 className="title">Add Items</h1>
             {flashMsg && (
                 <p className="text-error absolute top-24 right-6 text-sm shadow-lg rounded-2xl px-4 py-2">
                     {flashMsg}
